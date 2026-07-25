7ec180c890bd5ac49d1875b333e40de97de1418a
Serena Smith <serena_smith@dlsu.edu.ph>
2026-07-25 17:32:26 +0800

    Started ProfileController class.

---
+++ src/main/java/ph/edu/dlsu/lbycpob/profilemanagerapplication/controller/ProfileController.java
@@ -0,0 +1,34 @@
+package ph.edu.dlsu.lbycpob.profilemanager.controller;
+
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import ph.edu.dlsu.lbycpob.profilemanager.dto.Dtos;
+import ph.edu.dlsu.lbycpob.profilemanager.dto.Dtos.*;
+import ph.edu.dlsu.lbycpob.profilemanager.model.Profile;
+import ph.edu.dlsu.lbycpob.profilemanager.service.ProfileService;
+
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/profiles")
+public class ProfileController {
+
+    private final ProfileService profileService;
+
+    public ProfileController(ProfileService profileService) {
+        this.profileService = profileService;
+    }
+
+    @GetMapping
+    public List<Dtos.ProfileListItem> listProfiles() {
+        return profileService.listProfiles().stream().map(ProfileListItem::of).toList();
+    }
+
+    @GetMapping("/{id}")
+    public ProfileDetail getProfile(@PathVariable UUID id) {
+        Profile profile = profileService.getProfile(id);
+        return ProfileDetail.of(profile, profileService.getFriendsOf(id));
+    }
