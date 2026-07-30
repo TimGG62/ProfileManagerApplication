b30f10a655c0c52a23f2a97893788f9411c353dc
ken-xv <ken_rasing@dlsu.edu.ph>
2026-07-30 14:53:02 +0800

    Added an overridden toString method to print all of the profile's details as easy-to-read text.

--- src/main/java/ph/edu/dlsu/lbycpob/profilemanager/model/Profile.java
+++ src/main/java/ph/edu/dlsu/lbycpob/profilemanager/model/Profile.java
@@ -125,4 +125,15 @@ public class Profile {
         return Objects.hash(id, name, status, quote, picture, createdAt);
     }
 
+    @Override
+    public String toString() {
+        return "Profile{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", status='" + status + '\'' +
+                ", quote='" + quote + '\'' +
+                ", picture='" + picture + '\'' +
+                ", createdAt=" + createdAt +
+                '}';
+    }
 }
